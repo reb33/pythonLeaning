9b5c6d433e2ff34b2081d426afe7beb4fbcb7d1f
konstantin <konstbar@gmail.com>
2017-02-15 15:20:45 +0300

    list

---
+++ project 1/conditions2.py
@@ -0,0 +1,2 @@
+if True and 2/0>1:
+    print(True)
---
+++ project 1/list1.py
@@ -0,0 +1,12 @@
+listExem = [1 , 23, 43, 5, 3, 5, 3, "2", "wre", [2, 3, 4, 5]]
+
+listExem += ["Hi", "All"]
+listExem.append(['Everybody'])
+
+print(listExem)
+
+if ['Everybody'] in listExem:
+    print(True)
+
+print(listExem.count(5))
+print(len(listExem))
