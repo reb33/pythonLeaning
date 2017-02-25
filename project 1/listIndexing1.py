0db1273144b52e10225aa171ece05fa541d7f080
reb33 <konstbar@gmail.com>
2017-02-25 18:27:42 +0300

    listIndexing

---
+++ project 1/listIndexing1.py
@@ -0,0 +1,15 @@
+listExam = [1,2,3,4,5,6,7,8,9,10]
+
+print(listExam[5:-2])
+
+#reverse
+print(listExam[::-1])
+
+#четные чесла от 1 до 100
+print(list(range(1,101))[1::2])
+
+print(listExam[:-3:-1])
+print(listExam[-2:])
+
+#четные числа от 10
+print(listExam[::-2])
